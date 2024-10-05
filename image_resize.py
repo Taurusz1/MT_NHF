9c27fc4860495d9f8eb00d54880e444795c42f17
Taurusz1 <nyiribalazsszabolcs@gmail.com>
2024-10-05 10:10:32 +0200

    asd

--- image_resize.py
+++ image_resize.py
@@ -3,8 +3,8 @@ from PIL import Image
 
 def Img_Resize(old_folder_path, new_file_path, image_type):
     output_size = (256, 256)
-    if not os.path.exists(new_folder):
-        os.makedirs(new_folder)
+    if not os.path.exists(new_file_path):
+        os.makedirs(new_file_path)
     # Iterate through all files in the folder
     for i, filename in enumerate(os.listdir(old_folder_path), start=1):
         file_path = os.path.join(old_folder_path, filename)
@@ -13,7 +13,7 @@ def Img_Resize(old_folder_path, new_file_path, image_type):
             with Image.open(file_path) as img:
                 resized_img = img.resize(output_size)
                 new_filename = f"{image_type}_{i}.jpg"  # Change the extension based on your needs
-                new_file_path = os.path.join(new_folder, new_filename)
+                new_file_path = os.path.join(new_file_path, new_filename)
                 resized_img.save(new_file_path)
     
         except Exception as e:
