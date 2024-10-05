7fc944b9a2136dff296df26ebd493a83d5f4f3d1
Taurusz1 <nyiribalazsszabolcs@gmail.com>
2024-10-05 10:18:50 +0200

    Images Resized

--- image_resize.py
+++ image_resize.py
@@ -1,20 +1,20 @@
 import os
 from PIL import Image
 
-def Img_Resize(old_folder_path, new_file_path, image_type):
+def Img_Resize(old_folder_path, new_folder, image_type):
     output_size = (256, 256)
-    if not os.path.exists(new_file_path):
-        os.makedirs(new_file_path)
-    # Iterate through all files in the folder
+    
+    if not os.path.exists(new_folder):
+        os.makedirs(new_folder)
+        
     for i, filename in enumerate(os.listdir(old_folder_path), start=1):
         file_path = os.path.join(old_folder_path, filename)
 
         try:
             with Image.open(file_path) as img:
                 resized_img = img.resize(output_size)
-                new_filename = f"{image_type}_{i}.jpg"  # Change the extension based on your needs
-                new_file_path = os.path.join(new_file_path, new_filename)
-                resized_img.save(new_file_path)
-    
+                new_filename = f"{image_type}_{i}.jpg"
+                new_file = os.path.join(new_folder, new_filename)
+                resized_img.save(new_file)
         except Exception as e:
             print(f"Error processing file {filename}: {e}")
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from image_resize import Img_Resize
 
-Img_Resize('Books_Before_Resize', 'books', 'book')
+#Img_Resize('Books_Before_Resize', 'books', 'book')
 Img_Resize('Shoes_Before_Resize', 'shoes', 'shoe')
 Img_Resize('Chairs_Before_Resize', 'chairs', 'chair')
 Img_Resize('Fridges_Before_Resize', 'fridges', 'fridge')
