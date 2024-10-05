3943b1d367b94f22637a28fec7d77a87b0a911e6
Taurusz1 <nyiribalazsszabolcs@gmail.com>
2024-10-05 10:04:02 +0200

    finished function and folder renaming

--- image_resize.py
+++ image_resize.py
@@ -1,20 +1,20 @@
 import os
 from PIL import Image
 
-folder_path = 'Books_Before_Resize'
-new_folder = 'books'
-output_size = (256, 256)
+def Img_Resize(old_folder_path, new_file_path, image_type):
+    output_size = (256, 256)
+    if not os.path.exists(new_folder):
+        os.makedirs(new_folder)
+    # Iterate through all files in the folder
+    for i, filename in enumerate(os.listdir(old_folder_path), start=1):
+        file_path = os.path.join(old_folder_path, filename)
 
-# Iterate through all files in the folder
-for i, filename in enumerate(os.listdir(folder_path), start=1):
-    file_path = os.path.join(folder_path, filename)
-
-    try:
-        with Image.open(file_path) as img:
-            resized_img = img.resize(output_size)
-            new_filename = f"book_{i}.png"  # Change the extension based on your needs
-            new_file_path = os.path.join(new_folder, new_filename)
-            resized_img.save(new_file_path)
+        try:
+            with Image.open(file_path) as img:
+                resized_img = img.resize(output_size)
+                new_filename = f"{image_type}_{i}.jpg"  # Change the extension based on your needs
+                new_file_path = os.path.join(new_folder, new_filename)
+                resized_img.save(new_file_path)
     
-    except Exception as e:
-        print(f"Error processing file {filename}: {e}")
+        except Exception as e:
+            print(f"Error processing file {filename}: {e}")
--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-print("Hello, Docker!") 
+from image_resize import Img_Resize
+
+Img_Resize('Books_Before_Resize', 'books', 'book')
